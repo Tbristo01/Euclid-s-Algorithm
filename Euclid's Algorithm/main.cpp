c8093d33907a9b36f267673b78ff47ed5851610f
Tishaun Bristol <tishaunbristol@me.com>
2019-01-29 19:01:25 -0500

    Euclidean algorithm Recursive Version

--- Euclid's Algorithm/main.cpp
+++ Euclid's Algorithm/main.cpp
@@ -5,11 +5,28 @@
 //  Created by Tishaun Bristol on 1/29/19.
 //  Copyright © 2019 Bristol Developers. All rights reserved.
 //
+//  Find the greatest common denominator of two numbers
+//  Using Euclid's Algorithm
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//  function to find the GCD
+//  Recursive version
+
+int gcd (int a, int b)
+{
+    if (b == 0)
+        return a;
+    
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    //  Find GCD and Print to Screen
+    cout<<"GCD of 60,95 is "<<gcd(60,95)<<endl;
+    cout<<"GCD OF 20,8 is "<<gcd(20,8)<<endl;
+    
     return 0;
 }
